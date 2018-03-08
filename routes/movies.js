aeffa2ed80e84f89b99746568cc47795caa570e0
David Goodwin <goodwid@gmail.com>
2018-03-08 10:54:49 -0800

    Added random quote feature

---
+++ routes/movies.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import std404ErrMsg from '../lib/404';
+import Quote from '../models/quote';
+const router = express.Router(); // eslint-disable-line
+
+router
+  .get('/random/:movie', (req, res, next) => {
+    let movie = req.params.movie;
+    Quote
+      .find({movie: movie})
+      .lean()
+      .then(quotes => {
+        console.log('from random: ', quotes);
+        let numQuotes = quotes.length;
+        if (numQuotes === 0) next(std404ErrMsg);
+        if (quotes.length === 1) 
+          return res.send(quotes[0]);
+        let randomQuote = Math.floor(Math.random() * numQuotes);
+        res.send(quotes[randomQuote]);
+      })
+      .catch(err => next({
+        code: 404,
+        error: err,
+        msg: 'No quotes found',
+      }));
+  });
+
+
+export default router;
