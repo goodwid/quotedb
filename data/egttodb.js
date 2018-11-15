15417487a25f39ba3ce1c693f72337f156822113
goodwid <goodwid@gmail.com>
2018-11-15 11:20:24 -0800

    updated packages

--- data/egttodb.js
+++ data/egttodb.js
@@ -2,7 +2,37 @@
 
 const fs = require('fs');
 const mongoose = require('mongoose');
-require('../lib/setup-mongoose');
+const dbURI = process.env.MONGO_URI || 'mongodb://localhost/quotedb';
+
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose default connection open to ${dbURI}`); // eslint-disable-line
+});
+
+// If the connection throws an error
+mongoose.connection.on('error', (err) => {
+  console.log(`Mongoose default connection error: ${err}`); // eslint-disable-line
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose default connection disconnected'); // eslint-disable-line
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('Mongoose default connection disconnected through app termination'); // eslint-disable-line
+    process.exit(0);
+  });
+});
+
+
+mongoose.Promise = Promise;
+mongoose.connect(dbURI);
+
 const Quote = require('../models/quote');
 let fileTarget, movie;
 if (process.argv[2] && process.argv[3]) {
@@ -29,4 +59,8 @@ Promise.all(promises)
     process.exit(0);
   })
   .catch(err => console.error(err));
+
+
+
+
 
